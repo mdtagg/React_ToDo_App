82a00f074548c4fb28fed6978aee8a1fd792a3a9
Mike Tagg <mdtagg91@gmail.com>
2023-02-13 15:55:53 -0800

    light styling changes, adding state

--- src/components/UpcomingList.jsx
+++ src/components/UpcomingList.jsx
@@ -2,15 +2,15 @@
 const UpcomingList = () => {
     return (
         <div className='sidebar-group'>
-            <button className='button-container'>
+            <button>
                 <img className='todo-icon' src='icons/inbox.svg'></img>
                     Inbox
             </button>
-            <button className='button-container'>
+            <button>
                 <img className='todo-icon' src='icons/today.svg'></img>
                     Today
             </button>
-            <button className='button-container'>
+            <button>
                 <img className='todo-icon' src='icons/week.svg'></img>
                     This Week
             </button>
